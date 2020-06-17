8c8924f98b0a4a9ed147a510045ceab8cd41ea3b
UnaJeon <unajeon@gmail.com>
2020-06-17 20:08:26 +0000

    enhanced gradetable component

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,20 +1,50 @@
 class GradeTable{
-  constructor(tableElement){
+  constructor(tableElement,noGradesElement){
 this.tableElement = tableElement
+this.noGradesElement = noGradesElement
   }
 updateGrades(grades){
   var tbody = this.tableElement.querySelector('tbody')
   tbody.textContent = ''
   for (var i=0; i<grades.length; i++){
-    var tr = document.createElement('tr');
-    var tdName = document.createElement('td');
-    tdName.textContent = grades[i].name
-    var tdCourse = document.createElement('td');
-    tdCourse.textContent=grades[i].course
-    var tdGrade = document.createElement('td');
-    tdGrade.textContent=grades[i].grade
-    tbody.append(tr);
-    tr.append(tdName,tdCourse,tdGrade)
+  //   var tr = document.createElement('tr');
+  //   var tdName = document.createElement('td');
+  //   tdName.textContent = grades[i].name
+  //   var tdCourse = document.createElement('td');
+  //   tdCourse.textContent=grades[i].course
+  //   var tdGrade = document.createElement('td');
+  //   tdGrade.textContent=grades[i].grade
+  //   tbody.append(tr);
+  //   tr.append(tdName,tdCourse,tdGrade)
+  tbody.append(this.renderGradeRow(grades[i],this.deleteGrade))
+  }
+  if(!grades){
+    pElement.classList.remove('d-none')
+  }else{
+    pElement.className="d-none"
   }
 }
+onDeleteClick(deleteGrade){
+  this.deleteGrade= deleteGrade
+}
+renderGradeRow(data,deleteGrade){
+    var tableRow= document.createElement('tr')
+    var studentName = document.createElement('td');
+    studentName.textContent=data.name
+    var studnetCourse = document.createElement('td')
+    studnetCourse.textContent = data.course
+    var studentGrade = document.createElement('td');
+    studentGrade.textContent = data.grade
+    var buttonData= document.createElement('td')
+    var deleteButton = document.createElement('button')
+    deleteButton.textContent = "DELETE"
+    deleteButton.setAttribute("class","btn btn-danger" )
+    tableRow.append(studentName,studnetCourse,studentGrade,buttonData)
+    buttonData.append(deleteButton)
+
+    deleteButton.addEventListener('click',function(event){
+      deleteGrade(data.id)
+    })
+    return tableRow;
+ }
 }
